b5a5d91846a5f51a9baab535a57ab47039cb008f
unknown <sadoksassi2@gmail.com>
2023-02-14 00:13:03 +0100

    entity,crud-panier+test

--- DesktopApp/src/edu/esprit/main/Main.java
+++ DesktopApp/src/edu/esprit/main/Main.java
@@ -6,7 +6,9 @@
 package edu.esprit.main;
 
 import edu.esprit.entities.Competition;
+import edu.esprit.entities.Panier;
 import edu.esprit.services.ServiceCompetition;
+import edu.esprit.services.ServicePanier;
 import java.sql.Date;
 
 /**
@@ -15,6 +17,7 @@ import java.sql.Date;
  */
 public class Main {
     public static void main(String[] args) {
+        
         ServiceCompetition sc = new ServiceCompetition();
         Competition c =new Competition(1, "cadeau3", "p", "gsdhdghdghdg", "a9rani2", Date.valueOf("2023-04-14"), Date.valueOf("2023-04-23"));
         Competition c1 =new Competition(1,1, "cadeau1", "p1", "gsdhdghdghdg", "a9rani", Date.valueOf("2023-03-25"), Date.valueOf("2023-04-01"));
@@ -23,5 +26,17 @@ public class Main {
         //sc.delete(3);
         System.out.println(sc.getOneById(1));
         System.out.println(sc.getAll());
+        
+        
+        ServicePanier sp=new ServicePanier();
+        Panier p1=new Panier(1, 3, 25);
+        Panier p2=new Panier(1, 1, 2, 50);
+
+        //sp.ajouter(p1);
+        //sp.modifier(p2);
+        sp.delete(2);
+        
+        System.out.println(sp.getAll());
+        System.out.println(sp.getOneById(2));
     }
 }
