8a6ec1bd6419dfc466fb2047d86ced0a7acb751b
labidimalek14 <labidimalek@esprit.tn>
2023-02-13 22:49:52 +0100

    entity,crud competition+test

---
+++ DesktopApp/src/edu/esprit/entities/Competition.java
@@ -0,0 +1,117 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.esprit.entities;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author admin
+ */
+public class Competition {
+    private int id_competition; 
+    private int id_livre;
+    private String recompense; 
+    private String liste_participants; 
+    private String lien_competition; 
+    private String nom; 
+    private Date date_debut; 
+    private Date date_fin; 
+
+    public Competition() {
+    }
+
+    public Competition(int id_livre, String recompense, String liste_participants, String lien_competition, String nom, Date date_debut, Date date_fin) {
+        this.id_livre = id_livre;
+        this.recompense = recompense;
+        this.liste_participants = liste_participants;
+        this.lien_competition = lien_competition;
+        this.nom = nom;
+        this.date_debut = date_debut;
+        this.date_fin = date_fin;
+    }
+
+    public Competition(int id_competition, int id_livre, String recompense, String liste_participants, String lien_competition, String nom, Date date_debut, Date date_fin) {
+        this.id_competition = id_competition;
+        this.id_livre = id_livre;
+        this.recompense = recompense;
+        this.liste_participants = liste_participants;
+        this.lien_competition = lien_competition;
+        this.nom = nom;
+        this.date_debut = date_debut;
+        this.date_fin = date_fin;
+    }
+    
+    
+
+    public int getId_competition() {
+        return id_competition;
+    }
+
+    public int getId_livre() {
+        return id_livre;
+    }
+
+    public void setId_livre(int id_livre) {
+        this.id_livre = id_livre;
+    }
+
+    public String getRecompense() {
+        return recompense;
+    }
+
+    public void setRecompense(String recompense) {
+        this.recompense = recompense;
+    }
+
+    public String getListe_participants() {
+        return liste_participants;
+    }
+
+    public void setListe_participants(String liste_participants) {
+        this.liste_participants = liste_participants;
+    }
+
+    public String getLien_competition() {
+        return lien_competition;
+    }
+
+    public void setLien_competition(String lien_competition) {
+        this.lien_competition = lien_competition;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public Date getDate_debut() {
+        return date_debut;
+    }
+
+    public void setDate_debut(Date date_debut) {
+        this.date_debut = date_debut;
+    }
+
+    public Date getDate_fin() {
+        return date_fin;
+    }
+
+    public void setDate_fin(Date date_fin) {
+        this.date_fin = date_fin;
+    }
+
+    @Override
+    public String toString() {
+        return "Competition{" + "id_livre=" + id_livre + ", recompense=" + recompense + ", liste_participants=" + liste_participants + ", lien_competition=" + lien_competition + ", nom=" + nom + ", date_debut=" + date_debut + ", date_fin=" + date_fin + '}';
+    }
+    
+    
+    
+}
--- DesktopApp/src/edu/esprit/main/Main.java
+++ DesktopApp/src/edu/esprit/main/Main.java
@@ -5,8 +5,9 @@
  */
 package edu.esprit.main;
 
-import edu.esprit.util.DataSource;
-import java.sql.Connection;
+import edu.esprit.entities.Competition;
+import edu.esprit.services.ServiceCompetition;
+import java.sql.Date;
 
 /**
  *
@@ -14,6 +15,13 @@ import java.sql.Connection;
  */
 public class Main {
     public static void main(String[] args) {
-        Connection ds= DataSource.getInstance().getCnx();
+        ServiceCompetition sc = new ServiceCompetition();
+        Competition c =new Competition(1, "cadeau3", "p", "gsdhdghdghdg", "a9rani2", Date.valueOf("2023-04-14"), Date.valueOf("2023-04-23"));
+        Competition c1 =new Competition(1,1, "cadeau1", "p1", "gsdhdghdghdg", "a9rani", Date.valueOf("2023-03-25"), Date.valueOf("2023-04-01"));
+        //sc.ajouter(c);
+        //sc.modifier(c1);
+        //sc.delete(3);
+        System.out.println(sc.getOneById(1));
+        System.out.println(sc.getAll());
     }
 }
