4bd477ce13f62c681e8bbd41c04cb648d576cf43
labidimalek14 <labidimalek@esprit.tn>
2023-02-22 13:38:24 +0100

    interface maktabti + css

---
+++ DesktopApp/src/edu/esprit/gui/FXMLMaktabtiController.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.esprit.gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.shape.Rectangle;
+
+/**
+ * FXML Controller class
+ *
+ * @author admin
+ */
+public class FXMLMaktabtiController implements Initializable {
+
+    @FXML
+    private Hyperlink home;
+    @FXML
+    private Hyperlink library;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
