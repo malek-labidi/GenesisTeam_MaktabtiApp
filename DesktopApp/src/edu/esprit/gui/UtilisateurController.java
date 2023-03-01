73c43fbe8424cba8e623cb4ea20e4a6e90bb9dce
hachaniwassim <wassimhach16@gmail.com>
2023-03-01 23:13:23 +0100

    fixing interface and getuser method

--- DesktopApp/src/edu/esprit/gui/UtilisateurController.java
+++ DesktopApp/src/edu/esprit/gui/UtilisateurController.java
@@ -5,7 +5,13 @@
  */
 package edu.esprit.gui;
 
+import edu.esprit.entities.Competition;
+import edu.esprit.entities.Evenement;
+import edu.esprit.entities.Livre;
 import edu.esprit.entities.Utilisateur;
+import edu.esprit.services.ServiceCompetition;
+import edu.esprit.services.ServiceEvenement;
+import edu.esprit.services.ServiceLivre;
 import edu.esprit.services.ServiceUtilisateur;
 import edu.esprit.util.DataSource;
 import java.io.IOException;
@@ -14,6 +20,9 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -29,7 +38,10 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -38,6 +50,7 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -93,13 +106,20 @@ public class UtilisateurController implements Initializable {
     @FXML
     private Pane pnlOverview;
 
+    private List<Utilisateur> e1 = new ArrayList<>();
+    private int id;
+    @FXML
+    private TextField searchh;
+    @FXML
+    private Button reset;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         affiche();
-                useview.setOnMouseClicked(this::getuser);
+        useview.setOnMouseClicked(this::getUtilisateur);
     }    
 
   @FXML
@@ -149,6 +169,8 @@ public class UtilisateurController implements Initializable {
      public void affiche() {
         ServiceUtilisateur su = new ServiceUtilisateur();
         useview.setItems(FXCollections.observableArrayList(su.getAll()));
+        // Définir des cellules personnalisée pour afficher les informations sur l'utilisateur
+        useview.setCellFactory(list -> new UtilisateurListCell());
      }
     
     /* @FXML
@@ -178,6 +200,12 @@ public class UtilisateurController implements Initializable {
 
     @FXML
     private void updateuser(ActionEvent event) {
+        
+        int id = useview.getSelectionModel().getSelectedItem().getId();
+        System.out.println(id);
+        Dialog<ButtonType> dialog = new Dialog<>();
+        
+        
             if (cat_name.getText().isEmpty() || cat_prenom.getText().isEmpty() || cat_email.getText().isEmpty() || cat_tel.getText().isEmpty() || cat_password.getText().isEmpty() || cat_role.getText().isEmpty()  ) {
             Alert a = new Alert(Alert.AlertType.ERROR, "Aucun champ vide n'est accepté", ButtonType.OK);
             a.showAndWait();
@@ -192,12 +220,22 @@ public class UtilisateurController implements Initializable {
             Alert a = new Alert(Alert.AlertType.ERROR, "Mot de passe  invalide!", ButtonType.OK);
             a.showAndWait();   
         }
+       /* else if (cat_role.getText()!="Administrateur"  || cat_role.getText()!="Client"  ||cat_role.getText()!="Auteur" ) {
+            Alert a = new Alert(Alert.AlertType.ERROR, "Role invalide!", ButtonType.OK);
+            a.showAndWait();   
+        } */
         else {
+        dialog.setHeaderText("Voulez-vous vraiment modifier l'utilisateur N°" + id + "?");
+        ButtonType buttonTypeYes = new ButtonType("Oui", ButtonBar.ButtonData.YES);
+        ButtonType buttonTypeNo = new ButtonType("Non", ButtonBar.ButtonData.NO);
+        dialog.getDialogPane().getButtonTypes().addAll(buttonTypeYes, buttonTypeNo);
+
+        Optional<ButtonType> result = dialog.showAndWait();
+        if (result.isPresent() && result.get() == buttonTypeYes) {
             ServiceUtilisateur su = new ServiceUtilisateur();
             Utilisateur u = new Utilisateur(cat_name.getText(),cat_prenom.getText(),cat_email.getText(),cat_password.getText(),Integer.parseInt(cat_tel.getText()), cat_role.getText() ) {} ;
-            String var1=cat_name.getText();
-            u= useview.getSelectionModel().getSelectedItem();
-            su.modifierutilisateur(var1,u);
+            su.modifier(u);
+            System.out.println(u);
             affiche();
             cat_name.clear();
             cat_prenom.clear();
@@ -206,8 +244,10 @@ public class UtilisateurController implements Initializable {
             cat_tel.clear();
             cat_role.clear();
 
+            
             Alert a = new Alert(Alert.AlertType.CONFIRMATION, "User Updated Successfully !", ButtonType.OK);
             a.showAndWait();
+        }
             
                   /*  ServiceUtilisateur su = new ServiceUtilisateur();
 
@@ -241,6 +281,22 @@ public class UtilisateurController implements Initializable {
 
         }
     }
+    
+        public void getUtilisateur(MouseEvent event) {
+        int index = useview.getSelectionModel().getSelectedIndex();
+        Utilisateur u= new Utilisateur() {} ;
+
+
+        ServiceUtilisateur su = new ServiceUtilisateur();
+        u= useview.getSelectionModel().getSelectedItem();
+        System.out.println(u);
+        cat_name.setText(u.getNom());
+        cat_prenom.setText(u.getPrenom());
+        cat_email.setText(u.getEmail());
+        cat_password.setText(u.getMot_de_passe());
+        cat_tel.setText(Integer.toString(u.getnum_telephone()));
+        cat_role.setText(u.getRole());
+    }
 
     @FXML
     private void deleteuser(ActionEvent event) {
@@ -252,18 +308,69 @@ public class UtilisateurController implements Initializable {
             Alert a = new Alert(Alert.AlertType.CONFIRMATION, "User Deleted Successfully !", ButtonType.OK);
             a.showAndWait();
     }
-    void getuser(MouseEvent event) {
-       /* String selecteditItem = useview.getSelectionModel().getSelectedItem().toString();
-        SelectedIndex=useview.getSelectionModel().getSelectedItem();*/
-    int index = useview.getSelectionModel().getSelectedIndex();
-    if (index <= -1) {
-        return;
+
+
+    //Filtres
+        public void filter() {
+        
+        String s = searchh.getCharacters().toString();
+        ServiceUtilisateur su = new ServiceUtilisateur();
+        this.e1 = su.getAll();
+        
+        
+        if (!s.isEmpty()) {
+            //this.useview = ServiceUtilisateur.filterByName(this.useview, s);
+
+        }
     }
-    ServiceUtilisateur it=new ServiceUtilisateur();
+
+    @FXML
+    private void search(KeyEvent event) {
+        
+        filter();
+        affiche();
+    }
+
+    @FXML
+    private void onreset(ActionEvent event) {
+        cat_name.setText(null);
+        cat_prenom.setText(null);
+        cat_email.setText(null);
+        cat_password.setText(null);
+        cat_tel.setText(null);
+        cat_role.setText(null);
+    }
     
+    //Afficher  la ListView Bien Ordonnée
+      private class UtilisateurListCell extends javafx.scene.control.ListCell<Utilisateur> {
+        @Override
+        public void updateItem(Utilisateur Utilisateur, boolean empty) {
+            super.updateItem(Utilisateur, empty);
 
+            if (empty || Utilisateur == null) {
+                setText(null);
+                setGraphic(null);
+            } else {
+                Label nameLabel = new Label(Utilisateur.getNom());
+                nameLabel.setStyle("-fx-font-weight: bold;");
+                ServiceUtilisateur sl= new ServiceUtilisateur();
+                
+
+                Label idLabel = new Label("Nom: " + sl.getOneById(Utilisateur.getId()).getNom());
+                Label prenomlabel = new Label("Prenom: " + Utilisateur.getPrenom());
+                Label emaillabel = new Label("Email: " + Utilisateur.getEmail());
+                Label passwdlabel = new Label("Mot de passe: " + Utilisateur.getMot_de_passe());
+                Label numtellabel = new Label("Numéro de téléphone: " + Utilisateur.getnum_telephone());
+                Label rolelabel = new Label("Role: " + Utilisateur.getRole());
+
+                VBox vbox = new VBox(nameLabel, idLabel,prenomlabel, emaillabel, passwdlabel, numtellabel ,rolelabel );
+                vbox.setSpacing(5);
+
+                setGraphic(vbox);
+            }
+        }
     }
-
+      
 /*********************************************CONTROLE DE SAISIE***************************************************************************/    
      private boolean Validateemail() {
         Pattern p = Pattern.compile("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$");
