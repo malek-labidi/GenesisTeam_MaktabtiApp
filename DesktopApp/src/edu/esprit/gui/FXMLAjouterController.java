ac9d0fc19e956c04d821ccd0e88ddc3b45782d3d
unknown <sadok.sassi@esprit.tn>
2023-03-05 17:00:54 +0100

    ajout de ticket fxml+controller

--- DesktopApp/src/edu/esprit/gui/FXMLAjouterController.java
+++ DesktopApp/src/edu/esprit/gui/FXMLAjouterController.java
@@ -12,6 +12,7 @@ import edu.esprit.entities.Utilisateur;
 import edu.esprit.services.ServiceEvenement;
 import edu.esprit.services.ServiceLivre;
 import edu.esprit.services.ServiceUtilisateur;
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Date;
 import java.sql.SQLException;
@@ -23,7 +24,11 @@ import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
@@ -31,6 +36,8 @@ import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -70,8 +77,6 @@ public class FXMLAjouterController implements Initializable {
     private Button reset;
     @FXML
     private Button ajoutevent;
-    @FXML
-    private Button back;
     
    
 
@@ -172,7 +177,18 @@ public class FXMLAjouterController implements Initializable {
     }
 
     @FXML
-    private void back(ActionEvent event) {
+    private void back(MouseEvent event) {
+        try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("FXMLEvenement.fxml"));
+                Parent root = loader.load();
+                Scene scene = new Scene(root);
+                Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                stage.setScene(scene);
+                stage.show();
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage());
+            }
+        
     }
 
     
