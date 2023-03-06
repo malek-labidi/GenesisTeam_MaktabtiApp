0c20117ca49b999d0758e0365f8f27b0f0c4a6f1
AbdelazizLahmar <mohamedabdelaziz.lahmar@esprit.tn>
2023-03-06 18:51:21 +0100

    Fix a big error

--- DesktopApp/src/edu/esprit/entities/PanierLivre.java
+++ DesktopApp/src/edu/esprit/entities/PanierLivre.java
@@ -10,5 +10,52 @@ package edu.esprit.entities;
  * @author abdelazizlahmar
  */
 public class PanierLivre {
+    private  int id_livre;
+    private  int id_panier ;
+    private int id_client;
+    private int quantite;
+
+    public PanierLivre(int id_livre, int id_panier, int quantite) {
+        this.id_livre = id_livre;
+        this.id_panier = id_panier;
+        this.quantite=quantite;
+    }
+
+    public int getQuantite() {
+        return quantite;
+    }
+
+    @Override
+    public String toString() {
+        return "PanierLivre{" + "id_livre=" + id_livre + ", id_panier=" + id_panier + ", quantite=" + quantite + '}';
+    }
+
+    public void setQuantite(int quantite) {
+        this.quantite = quantite;
+    }
+    public int getId_client() {
+        return id_client;
+    }
+
+    public void setId_client(int id_client) {
+        this.id_client = id_client;
+    }
+
+
+    public int getId_livre() {
+        return id_livre;
+    }
+
+    public int getId_panier() {
+        return id_panier;
+    }
+
+    public void setId_livre(int id_livre) {
+        this.id_livre = id_livre;
+    }
+
+    public void setId_panier(int id_panier) {
+        this.id_panier = id_panier;
+    }
     
 }
